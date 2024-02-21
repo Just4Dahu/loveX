2b11e2891fc84461aada0b95caff1102393e82e0
pgerm <pgermain974@icloud.com>
2024-02-21 17:13:52 +0100

    npm test

---
+++ public/loveCalculator.test.js
@@ -0,0 +1,17 @@
+// loveCalculator.test.js
+const { calculateLovePercentage } = require('./loveCalculator'); // Assurez-vous d'exporter la fonction depuis loveCalculator.js
+
+// Mock de document.getElementById pour simuler le DOM
+document.getElementById = jest.fn((id) => {
+    return {
+        innerText: ''
+    };
+});
+
+test('le pourcentage d\'amour entre "melvin" et "geraldine" doit être 110%', () => {
+    // Exécuter la fonction avec les noms test
+    calculateLovePercentage('melvin', 'geraldine');
+
+    // Vérifier si le résultat est celui attendu
+    expect(document.getElementById("percentage").innerText).toBe(`Percentage : 110 %`);
+});
